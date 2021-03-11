73e90bc42e55a85cca910a305147b8a5cf617993
cespedesjuanc <cespedesjuanc@gmail.com>
2021-03-11 13:41:49 -0300

    modifying file  envioMail.php

--- envioMail.php
+++ envioMail.php
@@ -1,11 +1,17 @@
 <?php
+$destinatario = 'cespedesjuanc@gmail.com';
+//el correo al que va enviado el mensaje.
 
 $nombre = $_POST['nombre'];
 $mail = $_POST['mail'];
 $telephone = $_POST['telephone'];
 $description = $_POST['description'];
 
+$header = 'Enviado desde la pagina WEB_LYV';
+$mensajeCompleto = $description . "\nAtentamente: " . $nombre . "\n". $telephone . "\n" . $mail;
 
-mail('cespedesjuanc@gmail.com', $nombre, $mail, $telephone, $description);
+mail($destinatario, $nombre, $mensajeCompleto, $header);
+echo "<script> alert('correo enviado exitosamente') </script>";
+echo "<script> setTimeout(\"location.href='index.html'\",1000) </script>";
 
 ?>
